5d458239aac4f6afd372a533c7d6b87ad84d9df1
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 15:20:44 -0400

    [TESTING] Using previous task as a template

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a tree by in-order traversal
- * @tree: The binary tree to traverse
- * @func: The function to call for each node
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node 
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
