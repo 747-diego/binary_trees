3cb51cd5dc11fa4074a63babce376938e4ef31c6
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 16:20:25 -0400

    [UPDATED HEADER FILE]

--- binary_trees.h
+++ binary_trees.h
@@ -70,6 +70,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* Post-order traversal */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+/* Height */
+size_t binary_tree_height(const binary_tree_t *tree);
+
 
 
 
