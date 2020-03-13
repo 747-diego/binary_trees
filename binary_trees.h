def154012a9482067597c36da9a7b2fd5e155b03
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 12:55:37 -0400

    [PROTOTYPE] Added prototype to the header

--- binary_trees.h
+++ binary_trees.h
@@ -39,3 +39,6 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
+
+/* New Node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
