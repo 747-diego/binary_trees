75dbdab65c8aa9ecd9cb83545ec3b19abb38c8ee
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 14:37:48 -0400

    [UPDATED TREES]

--- binary_trees.h
+++ binary_trees.h
@@ -55,4 +55,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* Delete Binary Tree */
 void binary_tree_delete(binary_tree_t *tree);
 
+/* Is leaf ? */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif
