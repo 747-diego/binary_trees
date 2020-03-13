372a6e9cad8c5397c358c498209712dc54bf47c4
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 15:27:22 -0400

    [UPDATED HEADER]

--- binary_trees.h
+++ binary_trees.h
@@ -67,5 +67,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /* In-order traversal */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
+/* Post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+
+
 
 #endif
