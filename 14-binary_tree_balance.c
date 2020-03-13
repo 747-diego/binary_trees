b3676a6fac46a6bf6362fca8b722a8b1a9fbcb83
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 19:01:49 -0400

    [BETTY CHECK]: White space

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measure the balance factor of a binary tree
+ * @tree: tree at hand
+ * Return: 0, or measure
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t LeftLeaf = 0;
+	size_t RightLeaf = 0;
+	size_t SubTotal = 0;
+	const binary_tree_t *BinaryNode = tree;
+
+	if (BinaryNode == NULL || (!BinaryNode->left && !BinaryNode->right))
+		return (0);
+
+	else if (!BinaryNode->left || !BinaryNode->right)
+		return (-1);
+	else
+		return (2);
+
+	LeftLeaf = binary_tree_balance(BinaryNode->left);
+	RightLeaf = binary_tree_balance(BinaryNode->right);
+
+	SubTotal = LeftLeaf - RightLeaf;
+	return (SubTotal);
+}
