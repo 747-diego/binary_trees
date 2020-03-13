f0cd53ff3a28685c5219520db1beabac39e77cd1
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 12:54:11 -0400

    [CHMOD EXEXUTABLE]

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - creates a node in the binary tree
- * @parent: parent node
- * @value: value to be stored in new node
- * Return: a pointer to the new node or NULL
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: a pointer to the parent node of the node to create
+ * @value:  is the value to put in the new node
+ * Return: a pointer to the new node or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
