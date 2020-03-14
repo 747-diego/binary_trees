1e3fc091e3e96c274ca6eec5f9e0aa3eae2b1abe
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-13 20:16:07 -0400

    [TESTING UNCLE NODE]

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,41 @@
+/**
+ * binary_tree_uncle - a function that finds the uncle of a node
+ * @node: a pointer to the node to find the uncle
+ * Return: a pointer to the uncle node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    binary_tree_t *UncleNode = node->parent;
+
+	if (node == NULL || UncleNode == NULL)
+		return (NULL);
+	return (binary_tree_sibling(UncleNode));
+}
+
+/**
+ * binary_tree_sibling - a function that finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Return: a pointer to the sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *BinaryNode;
+
+	BinaryNode = node;
+	if (!BinaryNode || !BinaryNode->parent)
+		return (NULL);
+
+	if (BinaryNode->parent->left && BinaryNode->parent->left != BinaryNode)
+	{
+		return (BinaryNode->parent->left);
+	}
+
+	if (BinaryNode->parent->right && BinaryNode->parent->right != BinaryNode)
+	{
+		return (BinaryNode->parent->right);
+	}
+
+	return (NULL);
+}
